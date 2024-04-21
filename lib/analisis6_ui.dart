f08c319715a34f1bdfe899b9ef8c8843373f4071
Irene <irene.torres@axians.es>
2024-04-21 14:07:46 +0200

    csv added

--- lib/analisis6_ui.dart
+++ lib/analisis6_ui.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:teaf_app/analisis5_ui.dart';
-import 'analisis4_ui.dart';
 import 'welcome_ui.dart';
 import 'sign_ui.dart';
 import 'solucion_ui.dart';
